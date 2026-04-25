5b52ded2cba39fab17c5a1a378d13b5bc501c958
Victor <dorosvictor@gmail.com>
2026-04-25 10:19:51 +0300

    Add option to run on multiple environmanets and browsers locally and in github actions.

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,19 +3,11 @@ import dotenv from 'dotenv'
 
 dotenv.config({ path: '.env.credentials' })
 
-// Load .env only if present (local dev)
+const environments = {
+  testing: 'https://todo.qacart.com/',
+  staging: 'https://todo.qacart.com/',
+}
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   timeout: 10000,
   testDir: './tests',
@@ -31,55 +23,57 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'https://todo.qacart.com/',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
   projects: [
+    // npx playwright test --project=staging
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: 'testing',
+      use: {
+        ...devices['Desktop Chrome'],
+        baseURL: environments.testing,
+      },
+    },
+    {
+      name: 'staging',
+      use: {
+        ...devices['Desktop Chrome'],
+        baseURL: environments.staging,
+      },
+    },
+
+    // ✅ Explicit combinations
+    {
+      name: 'testing-firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+        baseURL: environments.testing,
+      },
+    },
+    {
+      name: 'staging-firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+        baseURL: environments.staging,
+      },
     },
 
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: 'testing-webkit',
+      use: {
+        ...devices['Desktop Safari'],
+        baseURL: environments.testing,
+      },
     },
-
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: 'staging-webkit',
+      use: {
+        ...devices['Desktop Safari'],
+        baseURL: environments.staging,
+      },
     },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 })
